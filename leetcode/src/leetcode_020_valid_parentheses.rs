2fa10d0ba9b78aecf55980eeca223f6918dfa348
maomao <2892798998@qq.com>
2022-12-07 15:19:02 +0800

    feat(leetcode): leetcode_020_valid_parentheses.rs

--- leetcode/src/leetcode_020_valid_parentheses.rs
+++ leetcode/src/leetcode_020_valid_parentheses.rs
@@ -1,6 +1,8 @@
 /// leetcode 020
 ///
 /// [20.有效的括号](https://leetcode.cn/problems/valid-parentheses/)
+///
+/// 栈
 pub fn is_valid(s: String) -> bool {
     if s.len() % 2 != 0 {
         return false;
