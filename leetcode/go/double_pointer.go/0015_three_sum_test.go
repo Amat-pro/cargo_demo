9122af14cbdfa4ab17dc336f647c0c6dbe88cda7
maomao <2892798998@qq.com>
2023-01-28 17:54:03 +0800

    feat(leetcode): double-pointer-0015-三数之和

---
+++ leetcode/go/double_pointer.go/0015_three_sum_test.go
@@ -0,0 +1,11 @@
+package double_pointer
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_three_sum(t *testing.T) {
+	// [ [-1, 0, 1], [-1, -1, 2] ]
+	fmt.Println("==> ", threeSum([]int{-1, 0, 1, 2, -1, -4}))
+}
