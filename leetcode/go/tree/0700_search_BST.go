02a36562dff2bd4510102c47c60b8fe8159ce2b4
maomao <2892798998@qq.com>
2023-01-31 16:36:16 +0800

    feat(leetcode): 二叉树-700-二叉搜索树中的搜索

---
+++ leetcode/go/tree/0700_search_BST.go
@@ -0,0 +1,13 @@
+package tree
+
+// 二叉搜索树中的搜索
+// 递归法
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil || root.Val == val {
+		return root
+	}
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+	return searchBST(root.Right, val)
+}
