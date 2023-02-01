dd2b81ce5b75d5b8901b48daf3a7bbe7f493bddb
maomao <2892798998@qq.com>
2023-02-01 14:25:38 +0800

    nothing

--- leetcode/go/tree/todos.go
+++ leetcode/go/tree/todos.go
@@ -2,7 +2,7 @@ package tree
 
 // 701.二叉搜索树中的插入操作
 
-// 450.删除二叉搜索树中的节点
+// 450.删除二叉搜索树中的节点 (see: https://www.bilibili.com/video/BV1tP41177us/?share_source=copy_web&vd_source=cc141d8be9451fa7758d9fc60e7bceb3)
 
 // 669. 修剪二叉搜索树
 
