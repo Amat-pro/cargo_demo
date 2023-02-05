248adb51abf97c8616a38bc1c688234fbc8e77a1
maomao <2892798998@qq.com>
2023-02-05 18:13:56 +0800

    feat(leetcode): DP-0213-打家劫舍

---
+++ leetcode/go/rob/0213_rob.go
@@ -0,0 +1,15 @@
+package rob
+
+func rob_ii(nums []int) int {
+
+	if len(nums) == 0 {
+		return 0
+	}
+
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	return max(rob(nums[:len(nums)-1]), rob(nums[1:]))
+
+}
---
+++ leetcode/go/rob/0213_rob_test.go
@@ -0,0 +1,12 @@
+package rob
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_rob_ii(t *testing.T) {
+
+	// 3
+	fmt.Println("==> ", rob_ii([]int{2, 3, 2}))
+}
